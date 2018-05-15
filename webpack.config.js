5007cfd1b3a01ec0a93ea6e6c13c52837a822828
Tina Giska <tina.tylicka@gmail.com>
2018-05-15 10:26:33 +0200

    Update task

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = (env) => {
 
     return {
         mode: environment,
-        entry: './src/app.js',
+        entry: './src/index.js',
         output: {
             path: path.resolve(__dirname, 'build'),
             filename: 'app.' + environment + '.bundle.js'
@@ -31,7 +31,7 @@ module.exports = (env) => {
           }
       ]
     },
-    optymalization: {
+    optimization: {
       minimize: false
 },
 }
